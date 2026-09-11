6496edd72ea3c20c264b7b4648af508ae5f560c5
Tom Oosterbaan <2159360@talnet.nl>
2026-09-11 10:49:21 +0200

    Create: formulier en store

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -21,16 +21,30 @@ class GameController extends Controller
      */
     public function create()
     {
-        //
+        return view('games.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'game_name' => 'required',
+            'platform' => 'required',
+            'genre' => 'required',
+            'rating' => 'required|numeric|min:0|max:10'
+        ]);
+
+        $game = new Game([
+            'game_name' => $request->get('game_name'),
+            'platform' => $request->get('platform'),
+            'genre' => $request->get('genre'),
+            'rating' => $request->get('rating')
+        ]);
+
+        $game->save();
+
+        return redirect('/games')->with('success', 'Game added!');
     }
+
 
     /**
      * Display the specified resource.
